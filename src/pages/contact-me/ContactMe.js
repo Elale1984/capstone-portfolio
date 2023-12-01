58cc4bcf0c7ddcd79c89c8be93ef264c8e0aa21e
Edward <edward.gratefuldead1@gmail.com>
2023-12-01 01:28:06 -0600

    Working on contact me page

--- src/pages/contact-me/ContactMe.js
+++ src/pages/contact-me/ContactMe.js
@@ -3,7 +3,18 @@ import './ContactMe.css'
 
 const ContactMe = () => {
   return (
-    <div>ContactMe</div>
+    <div contact-me-container>
+      <div className="contact-me-header">
+        <p className="header-text">Let me show you why I am worth it. </p>
+      </div>
+      <div className="contact-me-form">
+        <input type="text" className="contact-me-form-control" placeholder='Name'/>
+        <input type="text" className="contact-me-form-control" placeholder='Email'/>
+        <textarea  type="text" cols="40" rows="5" className="contact-me-form-control" placeholder='Leave me a message!'/>
+        <input type="text" className="contact-me-form-control" />
+        <div className="contact-me-submit-button">Send</div>
+      </div>
+    </div>
   )
 }
 
