30fdff10c28e4e129bdf02a9e18b8166f06d410a
Edward <edward.gratefuldead1@gmail.com>
2023-12-01 19:51:45 -0600

    ContactMe done. Email can be send through emailJs.

--- src/pages/contact-me/ContactMe.js
+++ src/pages/contact-me/ContactMe.js
@@ -1,21 +1,68 @@
-import React from 'react'
-import './ContactMe.css'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./ContactMe.css";
 
-const ContactMe = () => {
+export const ContactMe = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_n6mis7z",
+        "roomz-template_sm309opa#",
+        form.current,
+        "jAcDsHEBBavXod1ZA"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div contact-me-container>
+    <div className="contact-me-container">
       <div className="contact-me-header">
-        <p className="header-text">Let me show you why I am worth it. </p>
+        <p className="header-text">Get in touch...</p>
       </div>
-      <div className="contact-me-form">
-        <input type="text" className="contact-me-form-control" placeholder='Name'/>
-        <input type="text" className="contact-me-form-control" placeholder='Email'/>
-        <textarea  type="text" cols="40" rows="5" className="contact-me-form-control" placeholder='Leave me a message!'/>
-        <input type="text" className="contact-me-form-control" />
-        <div className="contact-me-submit-button">Send</div>
-      </div>
+      <form ref={form} className="contact-me-form" onSubmit={sendEmail}>
+        <input
+          type="text"
+          className="contact-me-form-control"
+          placeholder="Name"
+          required
+          name="name"
+        />
+        <input
+          type="text"
+          className="contact-me-form-control"
+          placeholder="Email"
+          required
+          name="email"
+        />
+        <input type="tel" name="phone" className="contact-me-form-control" placeholder="Phone (Optional)" />
+        <textarea
+          type="text"
+          cols="40"
+          rows="5"
+          className="contact-me-form-control"
+          placeholder="Leave me a message!"
+          required
+          name="message"
+        />
+        <input
+          type="submit"
+          className="contact-me-submit-button"
+          value="Message Me"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactMe
+export default ContactMe;
