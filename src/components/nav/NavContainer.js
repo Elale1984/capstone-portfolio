00d1575d47fcc7a42ed6a264410fe60c83fde4c9
Edward <edward.gratefuldead1@gmail.com>
2023-12-04 00:11:08 -0600

    responsive work

--- src/components/nav/NavContainer.js
+++ src/components/nav/NavContainer.js
@@ -1,26 +1,34 @@
 import React from "react";
 import "./NavContainer.css";
 import { Link, useLocation } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavContainer = ({ containerClass, linkClass }) => {
   const { pathname } = useLocation();
 
   return (
     <div className={containerClass}>
-      {pathname !== "/" && (
-        <Link to="/" className={linkClass}>
-          Home
+      <div>
+        {pathname !== "/" && (
+          <Link to="/" className={linkClass}>
+            Home
+          </Link>
+        )}
+        <Link to="/overview" className={linkClass}>
+          Overview
         </Link>
-      )}
-      <Link to="/overview" className={linkClass}>
-        Overview
-      </Link>
-      <Link to="/features" className={linkClass}>
-        Features
-      </Link>
-      <Link to="/contact-me" className={linkClass}>
-        Lets Talk
-      </Link>
+        <Link to="/features" className={linkClass}>
+          Features
+        </Link>
+        <Link to="/contact-me" className={linkClass}>
+          Lets Talk
+        </Link>
+      </div>
+      <span className="top-nav-small-container">
+        <p>asdf j;lkasdhf ;alsdkhjfg;aslfh;asdihfwoipefj'alsdkjfg;lasdkfgnl;'adfhv'alsfjn'al;sjigfv'
+          a;elsdkfgjmv'l;ks
+        </p>
+      </span>
     </div>
   );
 };
